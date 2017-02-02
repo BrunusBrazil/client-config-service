fb82322a2758b8a5b56c4bc1bb6066ec433574c5
BrunuusBrazil <brunocardoso.fatec.itu@gmail.com>
2017-02-02 23:55:06 +0100

    initial commit

---
+++ src/main/java/microservice/spring/cloud/config/ClientConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package microservice.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConfigServiceApplication.class, args);
+	}
+}
